e32f0f4b6819d0a93f3daaa946932337378be76f
Shikakiben <108907095+Shikakiben@users.noreply.github.com>
2025-11-20 09:47:47 +0100

    Décommente installation de pika-backup dans le script

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm pika-backup
+pacman -Syu --noconfirm pika-backup
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
